c3d9269e1680d4f04144e3b2ae49d89007b73dba
carla assis <assiscarla89@gmail.com>
2021-02-23 09:11:57 -0300

    Alteracao da classe de teste para adicionar lista

--- src/main/java/br/edu/infnet/appsdim/aula01/ExemploTeste.java
+++ src/main/java/br/edu/infnet/appsdim/aula01/ExemploTeste.java
@@ -1,13 +1,29 @@
 package br.edu.infnet.appsdim.aula01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExemploTeste {
 
 	public static void main(String[] args) {
-
+		
+		String[] nomes = {"Carla", "Maria", "Jose"};
+		
+		String[] emails = {"carla@gmail.com, maria@gmail.com, jose@gmail.com"};
+		
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		//criar objetos: pulando a coleção
+		for(int i=0; i < nomes.length; i++) {
 		Aluno aluno = new Aluno();
-		aluno.setId(1);
-		aluno.setNome("Carla");
-		aluno.setEmail("carla.chagas@al.infnet.edu.br");
-		System.out.println(aluno);
+		aluno.setId(i+1);
+		aluno.setNome(nomes[i]);
+		aluno.setEmail(emails[i]);
+		alunos.add(aluno);
+		}
+		//leitura da coleção
+		for(Aluno item : alunos) {
+			System.out.println(item);
+		}
 	}
 }
